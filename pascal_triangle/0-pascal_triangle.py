40013c876d8d636387fc74f619cb9ee51bf8bbd4
Elijahbk <b.kaneza@alustudent.com>
2024-02-28 11:14:11 +0000

    second

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Function returning the Pascal triangle of n"""
+"""Create a function that returns the Pascal’s triangle of n"""
+
+
 def pascal_triangle(n):
+    """Returns pascal triangle of n"""
     if n <= 0:
         return []
     triangle = [[1]]
